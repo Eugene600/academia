b4185730b33f90a503c07c011a614b1d35734238
Erick <hearteric57@gmail.com>
2025-03-26 10:50:41 +0300

    chore: added android files to merge student performance pages

--- android/app/.cxx/Debug/122r5g24/arm64-v8a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
+++ android/app/.cxx/Debug/122r5g24/arm64-v8a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.13.2-arch1-1")
+set(CMAKE_HOST_SYSTEM "Linux-6.13.7-arch1-1")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.13.2-arch1-1")
+set(CMAKE_HOST_SYSTEM_VERSION "6.13.7-arch1-1")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/erick/Android/Sdk/ndk/27.0.12077973/build/cmake/android.toolchain.cmake")
