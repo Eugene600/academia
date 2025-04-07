dcdd016ecf947ba317b8bac10482651ee049beec
Erick <hearteric57@gmail.com>
2025-04-07 22:35:00 +0300

    fix: exam timetable feature missing export

--- lib/features/exam_timetable/exam_timetable.dart
+++ lib/features/exam_timetable/exam_timetable.dart
@@ -1,5 +1,4 @@
 export 'widgets/widgets.dart';
 export 'models/exam.dart';
-export 'controllers/exam_timetable_controller.dart';
 export 'views/exam_timetable_page.dart';
 export 'bloc/exam_bloc.dart';
